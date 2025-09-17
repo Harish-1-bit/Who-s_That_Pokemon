f8f96fc58c75d1ba8039f3343fb7e096a50f33ea
Harish <soniharish029@gmail.com>
2025-09-17 17:06:04 +0530

    done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -71,7 +71,7 @@ const Card = () => {
         <div className=' duration-200 text-4xl mx-1 font-bold  font-stretch-95%'>
             <div className="border border-gray-300 rounded-md p-4 my-4 flex items-center justify-center relative">
             <img  className='h-52 brightness-0' src={pokemon?.imageURL} alt="pokemon" />
-            <button onClick={handleHint} className={'absolute right-5 top-5 text-2xl px-5 py-2 border-2 font-mono bg-amber-500 rounded-4xl'}>Hints?</button>
+            <button onClick={handleHint} className={'absolute bottom-2 right-2 text-lg px-3 md:right-5 md:top-5 md:text-2xl md:h-16 md:px-8 border-2 font-mono bg-amber-500 rounded-4xl'}>Hints?</button>
         </div>
           <p className={hint?'':'hidden'}>
             <p>Abilities:-</p>
@@ -79,7 +79,7 @@ const Card = () => {
                 pokemon.ability.map((abi)=>{
            console.log(abi)
            return (
-                <span className={'duration-200 text-2xl text-gray-500 mr-2 my-2 font-bold pos uppercase'}>{`${abi.ability.name}`}.</span>
+                <span className={'duration-200 text-xl  md:text-2xl text-gray-500 mr-2 my-2 font-bold pos uppercase'}>{`${abi.ability.name}`}.</span>
                 
 
            )  
